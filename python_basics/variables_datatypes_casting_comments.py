bd17d844d0793f03eeb9263629880f05b26d8a78
21rawatk-netizen <21rawatk@gmail.com>
2026-01-11 01:35:32 +0530

    Update and rename variables_loops_function.py to variables_datatypes_casting_comments.py

---
+++ python_basics/variables_datatypes_casting_comments.py
@@ -0,0 +1,27 @@
+#variables
+a=2 #integer
+name='kiran' #string
+b=True #boolean
+print(a)
+print(name)
+print(b)
+# data type checker
+print(type(a))
+print(type(name))
+print(type(b))
+#datatypes
+w=['a','b','c','d'] # list
+print(w)
+x=('a','b','c','d') # tuple
+print(x)
+y={'a','b','c','d'} # set
+print(y)
+z={                  # dict
+    "name":"kiran",
+    "age":36
+}
+print(z)
+# casting
+c=str(a)
+print(c) # this will be '2'
+'''multiline comments'''
