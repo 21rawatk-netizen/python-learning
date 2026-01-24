5bb3fadf8c57b3e07f70a44127018d84d91f9a36
21rawatk-netizen <21rawatk@gmail.com>
2026-01-24 23:32:43 +0530

    Create 3.Number_operations&type_casting.py

---
+++ python_basics/3.Number_operations&type_casting.py
@@ -0,0 +1,27 @@
+a=10
+b=-0.5
+print(type(a))
+print(type(b))
+print(a+b)
+print(10/5)
+print((10+2)*3)
+#Absolute value
+print(abs(-10))
+#round off(value to round off,no. of value to round off)
+print(round(5.67))
+print(round(5.678,2))
+# max and min
+print(min(4,2,7))
+print(max(4,2,9))
+#power(number,power)
+print(pow(2,3))
+# sum of numbers in list and tuple
+nums=[10,20,30]
+print(sum(nums))
+# divmod function gives quotient and remainder together
+print(divmod(10,3))
+#Typecasting
+print(int(5.9))
+print(float(10))
+print(complex(2,3))
+
